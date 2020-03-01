044b833e466413dff0d6e14215f6ca4fd3592a55
dineshs7 <s.dineshcomputer@gmail.com>
2020-03-01 13:28:26 +0530

    Deploying it onto Github pages

---
+++ frontend/build/precache-manifest.d5dfc561bb01f8fa000155c5fcab2995.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c688cf13bc352498ace87e912e73c9f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62694196a0a4383decb4",
+    "url": "/static/css/main.c3484e98.chunk.css"
+  },
+  {
+    "revision": "de707741dc5e801a30d7",
+    "url": "/static/js/2.05ec3892.chunk.js"
+  },
+  {
+    "revision": "50bd2817bd3e9575372fcf6fcfa89ca3",
+    "url": "/static/js/2.05ec3892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62694196a0a4383decb4",
+    "url": "/static/js/main.566f4773.chunk.js"
+  },
+  {
+    "revision": "add59df889169e134360",
+    "url": "/static/js/runtime-main.9c46f2d5.js"
+  }
+]);
